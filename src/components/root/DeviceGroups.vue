f2ac57783f84f4d191fe0fe30f7af59ded970118
Igogrek <igogrek@gmail.com>
2018-04-27 18:27:46 +0300

    Added modals for actions

--- src/components/root/DeviceGroups.vue
+++ src/components/root/DeviceGroups.vue
@@ -1,83 +1,139 @@
 <template>
-  <Tree
-    :items="items"
-    @toggled="toggled">
-    <template slot="header">
-      <th>Name</th>
-      <th>Owner</th>
-      <th>Id</th>
-      <th/>
-    </template>
-    <template
-      slot="item-name"
-      slot-scope="{item}">
-      <span
-        class="item-name"
-        :style="{left: item.level * 20 + 'px' }">
+  <div>
+    <Tree
+      :items="items"
+      @toggled="toggled">
+      <template slot="header">
+        <th>Name</th>
+        <th>Owner</th>
+        <th>Id</th>
+        <th/>
+      </template>
+      <template
+        slot="item-name"
+        slot-scope="{item}">
         <span
-          class="level"
-          v-if="item.level"/>
-        <img
-          v-if="item.c8y_IsDevice"
-          class="group-icon"
-          src="./assets/device.svg">
-        <img
-          v-else
-          class="group-icon"
-          src="./assets/group.svg">
-        {{ item.name }}
-        <img
-          v-if="item.loading"
-          class="loading-icon"
-          src="../../assets/loading_outline.svg">
-      </span>
-    </template>
-    <template
-      slot="item-columns"
-      slot-scope="{item}">
-      <td>{{ item.owner }}</td>
-      <td>{{ item.id }}</td>
-      <td>
-        <Dropdown v-if="!item.c8y_IsDevice">
+          class="item-name"
+          :style="{left: item.level * 20 + 'px' }">
           <span
-            slot="toggle"
-            class="icon is-small">
-            <img
-              class="more-icon"
-              src="./assets/more.svg">
-          </span>
-          <template slot="items">
-            <a class="dropdown-item">
-              Add child group
-            </a>
-            <a class="dropdown-item">
-              Delete group
-            </a>
-          </template>
-        </Dropdown>
-      </td>
-    </template>
-  </Tree>
+            class="level"
+            v-if="item.level"/>
+          <img
+            v-if="item.c8y_IsDevice"
+            class="group-icon"
+            src="./assets/device.svg">
+          <img
+            v-else
+            class="group-icon"
+            src="./assets/group.svg">
+          {{ item.name }}
+          <img
+            v-if="item.loading"
+            class="loading-icon"
+            src="../../assets/loading_outline.svg">
+        </span>
+      </template>
+      <template
+        slot="item-columns"
+        slot-scope="{item}">
+        <td>{{ item.owner }}</td>
+        <td>{{ item.id }}</td>
+        <td>
+          <Dropdown v-if="!item.c8y_IsDevice">
+            <span
+              slot="toggle"
+              class="icon is-small">
+              <img
+                class="more-icon"
+                src="./assets/more.svg">
+            </span>
+            <template slot="items">
+              <a
+                class="dropdown-item"
+                @click="toggleAddModal">
+                Add child group
+              </a>
+              <a
+                class="dropdown-item"
+                @click="toggleDeleteModal">
+                Delete group
+              </a>
+            </template>
+          </Dropdown>
+        </td>
+      </template>
+    </Tree>
+    <ConfirmModal
+      v-if="isDeleteModalShown"
+      @exit="toggleDeleteModal"
+      @confirm="deleteGroup">
+      <template slot="modal-title">Delete group?</template>
+      <div
+        class="has-text-centered"
+        slot="modal-body">
+        <p class="is-size-6 popup-body">
+          Are you sure you want to delete this group?
+        </p>
+      </div>
+      <template slot="confirm-button">
+        <button
+          class="button is-primary"
+          slot="modal-buttons">
+          Delete group
+        </button>
+      </template>
+    </ConfirmModal>
+    <ConfirmModal
+      v-if="isAddModalShown"
+      @exit="toggleAddModal"
+      @confirm="addGroup">
+      <template slot="modal-title">Add new child group?</template>
+      <div
+        class="has-text-centered"
+        slot="modal-body">
+        <p class="is-size-6 popup-body">
+          Please specify a name for new child group
+        </p>
+        <input
+          class="input group-name"
+          type="text"
+          placeholder="Group name">
+      </div>
+      <template slot="confirm-button">
+        <button
+          class="button is-primary"
+          slot="modal-buttons">
+          Add group
+        </button>
+      </template>
+    </ConfirmModal>
+  </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import Tree, {ITreeItem} from '../common/Tree/Tree.vue';
   import Dropdown from '../common/Dropdown/Dropdown.vue';
+  import ConfirmModal from '../common/ConfirmModal/ConfirmModal.vue';
   import GroupService from '../../common/classes/service/GroupService';
   import {IGroup, IGroupResponse} from '../../common/interfaces/IGroup';
 
   interface IDeviceGroupData {
+    isDeleteModalShown: boolean;
+    isAddModalShown: boolean;
     items: ITreeItem[];
   }
 
   export default Vue.extend({
     components: {
       Tree,
-      Dropdown
+      Dropdown,
+      ConfirmModal
     },
     data(): IDeviceGroupData {
       return {
+        isAddModalShown: false,
+        isDeleteModalShown: false,
         items: []
       };
     },
@@ -101,15 +157,29 @@
       mapGroupToTreeItem(item: IGroup) {
         (item as ITreeItem).lazy = Boolean(item.childAssets.references.length);
         return item;
+      },
+      toggleDeleteModal() {
+        this.isDeleteModalShown = !this.isDeleteModalShown;
+      },
+      toggleAddModal() {
+        this.isAddModalShown = !this.isAddModalShown;
+      },
+      deleteGroup() {
+
+      },
+      addGroup() {
+
       }
     }
   });
 </script>
 
 <style lang="scss" scoped>
+  $icon-width: 16px;
+
   .group-icon {
-    width: 16px;
-    height: 16px;
+    width: $icon-width;
+    height: $icon-width;
     position: relative;
     top: 2px;
   }
@@ -117,8 +187,12 @@
   .more-icon {
     position: relative;
     top: -8px;
-    width: 16px;
+    width: $icon-width;
     transform: rotate(90deg);
+  }
+
+  .group-name {
+    margin-top: 20px;
   }
 </style>
 
